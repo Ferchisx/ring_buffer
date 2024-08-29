86c4a7d5c191d098e3803e1d1f07a865ceaa4683
Ferchisx <fefernandeza@unal.edu.co>
2024-08-29 09:12:40 -0500

    Implementing UART1
    Making changes to initialize uart1 and use two ring buffers, declaring structures and modifying functions in both libraries buffer.h and buffer.c

--- Core/Inc/ring_buffer.h
+++ Core/Inc/ring_buffer.h
@@ -9,12 +9,22 @@
 #define INC_RING_BUFFER_H_
 
 #include <stdint.h>
-uint8_t ring_buffer_size(void);
-uint8_t ring_buffer_is_full(void);
-uint8_t ring_buffer_is_empty(void);
-uint8_t ring_buffer_read(uint8_t * byte);
-void ring_buffer_reset();
-void ring_buffer_write(uint8_t data);
 
+typedef struct ring_buffer_{
+	uint8_t *buffer;
+	uint8_t head;
+	uint8_t tail;
+	uint8_t is_full;
+	uint8_t capacity;
+}ring_buffer_t;
+
+void ring_buffer_init(ring_buffer_t *rb, uint8_t mem_add, uint8_t capacity);
+void ring_buffer_write(ring_buffer_t *rb, uint8_t data);
+void ring_buffer_reset(ring_buffer_t *rb);
+
+uint8_t ring_buffer_size(ring_buffer_t *rb);
+uint8_t ring_buffer_is_full(ring_buffer_t *rb);
+uint8_t ring_buffer_is_empty(ring_buffer_t *rb);
+uint8_t ring_buffer_read(ring_buffer_t, uint8_t *byte);
 
 #endif /* INC_RING_BUFFER_H_ */
--- Core/Src/ring_buffer.c
+++ Core/Src/ring_buffer.c
@@ -8,75 +8,75 @@
 
 #include "ring_buffer.h"
 
-//para crear la memoria en buffer
-#define capacity (10)
-uint8_t ring_buffer[capacity];
-uint8_t head_ptr;
-uint8_t tail_ptr;
-uint8_t is_full; //bandera que indica cuando el buffer está lleno
+void ring_buffer_init(ring_buffer_t *rb, uint8_t * mem_add, uint8_t cap){
+	rb->buffer = mem_add;
+	rb->capacity = cap;
 
-void ring_buffer_write(uint8_t data){
-		ring_buffer[head_ptr] = data;
-		head_ptr = head_ptr + 1; //los espacios van a ir moviendose
+	ring_buffer_reset(rb);
+}
 
-		if(head_ptr >= capacity){ //indica el final del recorrido de la cabeza
-			head_ptr = 0;
+void ring_buffer_write(ring_buffer_t *rb, uint8_t data){
+		rb->buffer[rb->head] = data;
+		rb->head = rb->head + 1; //los espacios van a ir moviendose
+
+		if(rb->head >= rb->capacity){ //indica el final del recorrido de la cabeza
+			rb->head = 0;
 		}
 
-		if(is_full != 0){ //si se pierden datos viejos, para que la cola avance
-			tail_ptr = tail_ptr + 1;
+		if(rb->is_full != 0){ //si se pierden datos viejos, para que la cola avance
+			rb->tail = rb->tail + 1;
 		}
 
-		if(tail_ptr >= capacity){ //si la cola llega al final de la memoria
-			tail_ptr = 0; //se reinicia la bandera
+		if(rb->tail >= rb->capacity){ //si la cola llega al final de la memoria
+			rb->tail = 0; //se reinicia la bandera
 		}
 
-		if(head_ptr == tail_ptr){ //si la cabeza alcanza la cola
-			is_full = 1;
+		if(rb->head == rb->tail){ //si la cabeza alcanza la cola
+			rb->is_full = 1;
 		}
 }
 
-uint8_t ring_buffer_read(uint8_t *data){
-	if((is_full != 0) || (head_ptr != tail_ptr)){
-		*data = ring_buffer[tail_ptr];
-		tail_ptr = tail_ptr + 1;
-		if(tail_ptr >= capacity){
-			tail_ptr = 0;
+uint8_t ring_buffer_read(ring_buffer_t *rb, uint8_t *data){
+	if((rb->is_full != 0) || (rb->head != rb->tail)){
+		*data = rb->buffer[rb->tail];
+		rb->tail = rb->tail + 1;
+		if(rb->tail >= rb->capacity){
+			rb->tail = 0;
 		}
-		is_full = 0;
+		rb->is_full = 0;
 		return 1; //buffer con datos
 	}
 	return 0; //buffer vacío
 }
 
-void ring_buffer_reset(){
+void ring_buffer_reset(ring_buffer_t *rb){
 	//Queremos que el valor de la cabeza, cola y que la bandera de llenado queden en 0
-	head_ptr = 0;
-	tail_ptr = 0;
-	is_full = 0;
+	rb->head = 0;
+	rb->tail = 0;
+	rb->is_full = 0;
 }
 
-uint8_t ring_buffer_size(void){
+uint8_t ring_buffer_size(ring_buffer_t *rb){
 	uint8_t size = 0;
 
-    if (is_full != 0) {
-    	size = capacity; //Si el buffer se encuentra lleno, devuelve esa misma capacidad como tamaño
+    if (rb->is_full != 0) {
+    	size = rb->capacity; //Si el buffer se encuentra lleno, devuelve esa misma capacidad como tamaño
     }
 
-    if (head_ptr >= tail_ptr) {
-    	size = head_ptr - tail_ptr;	 //Si la cabeza es mayor o igual a la cola, el tamaño es la diferencia entre ambos,
+    if (rb->head >= rb->tail) {
+    	size = rb->head - rb->tail;	 //Si la cabeza es mayor o igual a la cola, el tamaño es la diferencia entre ambos,
         							 //ya que no se ha dado la vuelta al buffer
     } else {
-    	size = capacity + (head_ptr - tail_ptr); //Si la cabeza ha dado la vuelta y alcanza la cola, el tamaño será la
-        										 //capacidad del buffer más la diferencia entre cabeza y cola
+    	size = rb->capacity + (rb->head - rb->tail); //Si la cabeza ha dado la vuelta y alcanza la cola, el tamaño será la
+        										 	 //capacidad del buffer más la diferencia entre cabeza y cola
     }
     return size;
 }
 
-uint8_t ring_buffer_is_full(void){
-	return is_full;
+uint8_t ring_buffer_is_full(ring_buffer_t *rb){
+	return rb->is_full;
 }
 
-uint8_t ring_buffer_is_empty(void){
-	return ((head_ptr == tail_ptr) && (is_full == 0)) ?1:0;
+uint8_t ring_buffer_is_empty(ring_buffer_t *rb){
+	return ((rb->head == rb->tail) && (rb->is_full == 0)) ?1:0;
 }
